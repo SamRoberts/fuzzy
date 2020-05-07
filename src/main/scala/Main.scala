48a48188b6b7b254ae709bddc5b67a6204514c7b
Sam Roberts <sam.roberts.1983@gmail.com>
2020-05-07 17:16:33 +1000

    appeared to finished regex style kleene starts

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -13,8 +13,11 @@ object Main {
       ("aa.*bb", "afaffbb", 1),
       ("za*bb", "zbb", 0),
       ("a..*bb", "afaffbb", 0),
+      ("z(ac)*z", "zz", 0),
       ("z(ac)*z", "zacdacacz", 1),
-      ("z(a*c)*z", "zacdaaacaacz", 1)
+      ("z(a*c)*z", "zacdaaacaacz", 1),
+      (".*b.*c", "zzzzzbxxxxc", 0),
+      (".*b.*c", "yyyy", 2)
     )
 
     val results = cases.map { case (template, text, expected) => (template,  text, expected, Template(template).score(text)) }
@@ -39,13 +42,10 @@ object Main {
 
 case class Template(template: String, trace: Boolean = false) {
 
-  val forks      = createForks(template)
-  val traceForks = forks.length > 0 && forks.forall(_ < 10)
-
   def withTrace: Template = copy(trace = true)
 
-  // TODO add regex like repeating patterns rather that just wildcards
-  //      do this sooner rather than later in case it stuffs up logic
+  val flow = ControlFlow(template)
+
   // TODO implement matching. Could be higher priority, but how to implement matching?
   //      Can we record at each table index what the index of the next step we took was?
   //      Then how to recover match from that?
@@ -55,6 +55,7 @@ case class Template(template: String, trace: Boolean = false) {
   // TODO get benchmarks up and running and then start investigating whether we can make
   //      code more optimised and safer. Current approach is both overly simplistic but also
   //      more skeptical of creating new objects than standard Scala code.
+  // TODO Use a logical representation of template rather than raw template.
 
   def score(text: String): Int = {
     var stepCount = 0
@@ -70,11 +71,14 @@ case class Template(template: String, trace: Boolean = false) {
 
       printTraceMsg("enter", step, "from", parentStep, text, textIx, template, templateIx)
 
-      val templateFinished = templateIx >= template.length
-      val textFinished     = textIx >= text.length
-      def canFork          = set(forks(templateIx))
-      def bigForkIx        = (templateIx+1) max forks(templateIx)
-      def smallForkIx      = (templateIx+1) min forks(templateIx)
+      def templateFinished = templateIx >= template.length
+      def textFinished     = textIx >= text.length
+      def canFork          = set(forkIx)
+      def forkIx           = flow.forks(templateIx)
+      def mustGoto         = set(gotoIx)
+      def gotoIx           = flow.gotos(templateIx)
+      def nextIx           = templateIx+1
+      def nextText         = textIx+1
       def templateControl  = { val c = template(templateIx); c == '*' || c == '(' || c == ')' }
       def isMatch          = { val c = template(templateIx); c == '.' || c == text(textIx) }
 
@@ -98,32 +102,27 @@ case class Template(template: String, trace: Boolean = false) {
           // correct  whether textIx == or < than text.length
           text.length - textIx
         }
-        else if (!templateFinished && templateControl && !canFork) {
-          inner(step, textIx, templateIx+1, acc)
-        }
-        else if (!templateFinished && templateControl && canFork) {
-          minSet(inner(step, textIx, bigForkIx, acc), inner(step, textIx, smallForkIx, acc))
-        }
-        else if (!templateFinished && !templateControl && textFinished && !canFork) {
-          1 + inner(step, textIx, templateIx+1, acc+1)
-        }
-        else if (!templateFinished && !templateControl && textFinished && canFork) {
-          incSet(minSet(inner(step, textIx, bigForkIx, acc+1), inner(step, textIx, smallForkIx, acc+1)))
-        }
-        else if (!templateFinished && !templateControl && !textFinished && isMatch && !canFork) {
-          inner(step, textIx+1, templateIx+1, acc)
-        }
-        else if (!templateFinished && !templateControl && !textFinished && isMatch && canFork) {
-          minSet(inner(step, textIx+1, bigForkIx, acc), inner(step, textIx+1, smallForkIx, acc))
-        }
-        else if (!templateFinished && !templateControl && !textFinished && !isMatch && !canFork) {
-          incSet(minSet(inner(step, textIx, templateIx+1, acc+1), inner(step, textIx+1, templateIx, acc+1)))
-        }
-        else if (!templateFinished && !templateControl && !textFinished && !isMatch && canFork) {
-          incSet(minSet(inner(step, textIx, bigForkIx, acc+1), inner(step, textIx, smallForkIx, acc+1), inner(step, textIx+1, templateIx, acc+1)))
+        else if (mustGoto) {
+          inner(step, textIx, gotoIx, acc)
         }
         else {
-          throw new Exception("programmer error: unexpected case")
+          val forkResult = if (canFork) inner(step, textIx, forkIx, acc) else -1
+
+          val consumeResult =
+            if (templateControl) {
+              inner(step, textIx, nextIx, acc)
+            }
+            else if (textFinished) {
+              incSet(inner(step, textIx, nextIx, acc+1))
+            }
+            else if (isMatch) {
+              inner(step, nextText, nextIx, acc)
+            }
+            else {
+              incSet(minSet(inner(step, textIx, nextIx, acc+1), inner(step, nextText, templateIx, acc+1)))
+            }
+
+          minSet(forkResult, consumeResult)
         }
 
       if (set(result)) tableVal(textIx, templateIx) = result
@@ -145,66 +144,14 @@ case class Template(template: String, trace: Boolean = false) {
     if (set(score1) && set(score2)) score1 min score2
     else                            score1 max score2 // only works as unset numbers are negative
 
-  def minSet(score1: Int, score2: Int, score3: Int): Int =
-    minSet(minSet(score1, score2), minSet(score1, score3))
-
   def incSet(score: Int): Int =
     if (set(score)) score+1 else score
-
-  /** Returns an array of possible forks for each position, -1 if no fork */
-  def createForks(template: String): Array[Int] = {
-    val forks = Array.fill(template.length)(-1)
-    template.zipWithIndex.foldLeft(List.empty[Int]) { (scopes: List[Int], elem: (Char, Int)) =>
-      val (c, ix) = elem
-      (c, ix, ix+1 < template.length &&  template(ix+1) == '*', scopes) match {
-        case ('(', ix, true,  _) =>
-          throw new Exception(s"illegal (* pattern at ${Util.indexed(template, ix)}")
-
-        case ('(', ix, false, _) =>
-          ix +: scopes
-
-        case (')', ix, _, Nil) =>
-          throw new Exception(s"illegal ) at top level at ${Util.indexed(template, ix)}")
-
-        case (')', ix, true, scopeStart :: lowerScopes) =>
-          forks(scopes.head) = ix+1
-          forks(ix)          = scopeStart
-          lowerScopes
-
-        case (')', _, false, _ :: lowerScopes) =>
-          lowerScopes
-
-        case ('*', 0, _, _) =>
-          throw new Exception(s"illegal * at ${Util.indexed(template, 0)}")
-
-        case ('*', ix, true, _) =>
-          throw new Exception(s"illegal ** pattern at ${Util.indexed(template, ix)}")
-
-        case ('*', _, false, _) =>
-          scopes
-
-        case (c, ix, true, _) =>
-          forks(ix) = ix
-          scopes
-
-        case (c, _, _, _) =>
-          scopes
-      }
-    }
-
-    forks
-  }
 
   def printTraceMsg(action: String, step: Int, connector: String, lastStep: Int, text: String, textIx: Int, template: String, templateIx: Int): Unit  = {
     if (trace) {
       val splitText = Util.indexed(text, textIx)
       val splitTemplate = Util.indexed(template, templateIx)
-      if (traceForks) {
-        val splitForks = Util.indexed(forks.map(i => if (set(i)) i.toString else ".").mkString, templateIx)
-        println(f"$action $step%2d $connector%4s $lastStep%2d: position: $splitText%8s pattern: $splitTemplate%8s forks: $splitForks")
-      } else {
-        println(f"$action $step%2d $connector%4s $lastStep%2d: position: $splitText%8s pattern: $splitTemplate%8s")
-      }
+      println(f"$action $step%2d $connector%4s $lastStep%2d: position: $splitText%8s pattern: $splitTemplate%8s")
     }
   }
 
@@ -212,12 +159,7 @@ case class Template(template: String, trace: Boolean = false) {
     if (trace) {
       val splitText = Util.indexed(text, textIx)
       val splitTemplate = Util.indexed(template, templateIx)
-      if (traceForks) {
-        val splitForks = Util.indexed(forks.map(i => if (set(i)) i.toString else ".").mkString, templateIx)
-        println(f"$action $step%2d $connector%4s $lastStep%2d: position: $splitText%8s pattern: $splitTemplate%8s forks: $splitForks, $scoreType $score%2d")
-      } else {
-        println(f"$action $step%2d $connector%4s $lastStep%2d: position: $splitText%8s pattern: $splitTemplate%8s, $scoreType $score%2d")
-      }
+      println(f"$action $step%2d $connector%4s $lastStep%2d: position: $splitText%8s pattern: $splitTemplate%8s, $scoreType $score%2d")
     }
   }
 }
@@ -244,5 +186,93 @@ object Util {
   def indexed(text: String, ix: Int): String = {
     val spaced = text + " "
     spaced.substring(0, ix) + Console.BLUE_B + spaced(ix) + Console.RESET + spaced.substring(ix+1)
+  }
+}
+
+/** An array of possible forks and gotos for each position
+  *
+  * If we have a set fork, the possibilities at that index are to jump straight to
+  * the fork index without consuming character, or consume character and increment index.
+  *
+  * If we have a set goto, we must jump to the goto index straight away. It's up to this
+  * method to ensure we only assign this to control characters, as it will clobber any
+  * real characters we assign this to. (We really need a logical representation of the
+  * pattern rather than a raw string.)
+  *
+  * There are two basic patterns:
+  *
+  * index:   0123456789
+  * pattern: ab(cd)*ef
+  * forks:   ..7......
+  * gotos:   ......3..
+  *
+  * index:   0123456
+  * pattern: abc*de
+  * forks:   ..4...
+  * gotos:   ...2..
+  *
+  * forks at the beginning and end of the pattern are fine, as an index equal to length of
+  * pattern is allowed to indicate we are at the end:
+  *
+  * index:   01234
+  * pattern: abc*
+  * forks:   ..4.
+  * gotos:   ...2
+  *
+  * index:   01234
+  * pattern: a*bc
+  * forks:   2....
+  * gotos:   .0...
+  *
+  * Unclear if unstructured forks and gotos are best structure, vs. something that understands nesting.
+  */
+case class ControlFlow(forks: Array[Int], gotos: Array[Int])
+
+object ControlFlow {  
+  def apply(template: String): ControlFlow = {
+    val forks = Array.fill(template.length)(-1)
+    val gotos = Array.fill(template.length)(-1)
+
+    template.zipWithIndex.foldLeft(List.empty[Int]) { (scopes: List[Int], elem: (Char, Int)) =>
+      val (c, ix) = elem
+      (c, ix, ix+1 < template.length &&  template(ix+1) == '*', scopes) match {
+
+        case ('(', ix, true,  _) =>
+          throw new Exception(s"illegal (* pattern at ${Util.indexed(template, ix)}")
+
+        case ('(', ix, false, _) =>
+          ix +: scopes
+
+        case (')', ix, _, Nil) =>
+          throw new Exception(s"illegal ) at top level at ${Util.indexed(template, ix)}")
+
+        case (')', ix, true, scopeStart :: lowerScopes) =>
+          forks(scopeStart) = ix+2       // can fork past the asterix
+          gotos(ix+1)       = scopeStart // jump back at the asterix, for consistency with a* case
+          lowerScopes
+
+        case (')', _, false, _ :: lowerScopes) =>
+          lowerScopes
+
+        case ('*', 0, _, _) =>
+          throw new Exception(s"illegal * at ${Util.indexed(template, 0)}")
+
+        case ('*', ix, true, _) =>
+          throw new Exception(s"illegal ** pattern at ${Util.indexed(template, ix)}")
+
+        case ('*', _, false, _) =>
+          scopes
+
+        case (c, ix, true, _) =>
+          forks(ix)   = ix+2 // can fork past the asterix
+          gotos(ix+1) = ix   // jump back at asterix, fork will take as past this point
+          scopes
+
+        case (c, _, _, _) =>
+          scopes
+      }
+    }
+
+    ControlFlow(forks, gotos)
   }
 }
